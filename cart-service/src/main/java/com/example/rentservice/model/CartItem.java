de3246674d418e372f5444b701fed75b54035379
Veljko Maksimovic <maxi.maksimovic7@gmail.com>
2020-06-05 16:02:44 +0200

    Creating service and its model. Deleting rent service

---
+++ cart-service/src/main/java/com/example/rentservice/model/CartItem.java
@@ -0,0 +1,35 @@
+package com.example.rentservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long vehicle_id;
+
+    @Column(nullable = false)
+    private String vehicle_name;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Column(nullable = false)
+    private String owner_username;
+
+    @Column(nullable = false)
+    private Occupation time_span;
+}

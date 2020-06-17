7778f8fbff9850859fd92df1ee2dea69279c8474
Veljko Maksimovic <maxi.maksimovic7@gmail.com>
2020-06-17 20:56:32 +0200

    Cros origin header added

---
+++ zuul-api-gateway/src/main/java/com/example/zuulservice/filters/HeaderCORSFilter.java
@@ -0,0 +1,34 @@
+package com.example.zuulservice.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class HeaderCORSFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+
+        RequestContext ctx = RequestContext.getCurrentContext();
+
+        ctx.addZuulRequestHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+        return null;
+    }
+}

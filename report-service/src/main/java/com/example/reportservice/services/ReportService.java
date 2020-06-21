3e40cbdca8ad09dbf89b5f0eb8829ea554d6c092
Veljko Maksimovic <maxi.maksimovic7@gmail.com>
2020-06-22 00:14:38 +0200

    Admin can reject or approve comments and ratings

--- report-service/src/main/java/com/example/reportservice/services/ReportService.java
+++ report-service/src/main/java/com/example/reportservice/services/ReportService.java
@@ -1,10 +1,13 @@
 package com.example.reportservice.services;
 
 import com.example.reportservice.DTO.ReportDTO;
+import com.example.reportservice.ReportServiceApplication;
+import com.example.reportservice.enumeration.ReportStatus;
 import com.example.reportservice.model.VehicleReport;
 import com.example.reportservice.repository.ReportRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Set;
 
@@ -15,10 +18,21 @@ public class ReportService {
     private ReportRepository reportRepository;
 
     public Set<VehicleReport> getReportsForVehicle(Long vehicle_id){
-        Set<VehicleReport> vehicleReports = reportRepository.getReportsForVehicle(vehicle_id);
-        return vehicleReports;
+        return reportRepository.getReportsForVehicle(vehicle_id);
     }
 
+    public Set<VehicleReport> getPendingReports(){
+        return reportRepository.getPendingReports();
+    }
+
+    @Transactional
+    public void adminReportStatus(Long id, ReportStatus status){
+        if(status == ReportStatus.APPROVED){
+            reportRepository.adminReportStatus(id, 1);
+        }else{
+            reportRepository.adminReportStatus(id, 2);
+        }
+    }
     public void postReport(ReportDTO reportDTO, String username) {
         reportRepository.save(new VehicleReport(reportDTO, username));
     }
